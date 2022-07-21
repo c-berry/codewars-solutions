5fa0258270296bdd1d8c7605432e06f2da38096a
Christopher Berry <christopher.m.berry01@gmail.com>
2022-07-20 21:31:48 -0500

    complete daily kata

--- js/solutions.js
+++ js/solutions.js
@@ -668,3 +668,23 @@ function filter_list2(l) {
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
+// Given an array of integers, find the one that appears an odd number of times. There will always be only one integer that appears an odd number of times.
+function findOdd(numbers) {
+    let count = 0;
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = 0; j < numbers.length; j++){
+            if(numbers[i] === numbers[j]){
+                count++;
+            }
+        }
+        if(count % 2 !== 0 ){
+            return numbers[i];
+        }
+    }
+}
+// console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]));
+// console.log(findOdd([1,1,1,1,1,1,10,1,1,1,1]));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
