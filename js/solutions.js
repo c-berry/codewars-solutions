86af046b473b7168dc3fd1a6ce1a3b67161112d9
Christopher Berry <christopher.m.berry01@gmail.com>
2022-07-27 17:08:46 -0500

    complete daily katas

--- js/solutions.js
+++ js/solutions.js
@@ -775,6 +775,36 @@ function XO(str) {
     }
     return x === o;
 }
+// console.log(XO("oOxX"));
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
+// function which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side) =>
+function dirReduc(arr){
+    const result = []
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === "NORTH" && arr[i + 1] === "SOUTH" || arr[i] === "SOUTH" && arr[i + 1] === "NORTH" || arr[i] === "EAST" && arr[i + 1] === "WEST" || arr[i] === "WEST" && arr[i + 1] === "EAST"){
+            i++
+        } else {
+            if(result[result.length - 1] === "NORTH" && arr[i] === "SOUTH" || result[result.length - 1] === "SOUTH" && arr[i] === "NORTH" || result[result.length - 1] === "EAST" && arr[i] === "WEST" || result[result.length - 1] === "WEST" && arr[i] === "EAST"){
+                result.pop()
+            } else {
+                result.push(arr[i])
+            }
+        }
+    }
+    return result
+}
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
+// you are given a string of space separated numbers, and have to return the highest and lowest number. =>
+function highAndLow(numbers){
+    let numArr = numbers.split(" ");
+    let highest = Math.max(...numArr);
+    let lowest = Math.min(...numArr);
+    return highest + " " + lowest;
+}
+console.log(highAndLow("4 1 2 3 4 2 8"));
