baa9c5fae3731b1ecc941da47b18b39180f3f66e
Christopher Berry <christopher.m.berry01@gmail.com>
2022-08-12 14:39:11 -0500

    complete daily kata

--- js/solutions.js
+++ js/solutions.js
@@ -958,3 +958,16 @@ function betterThanAverage(classPoints, yourPoints) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+function findNextSquare(sq) {
+    // Return the next square if sq is a perfect square, -1 otherwise
+    if (Number.isInteger(Math.sqrt(sq)))
+        return (Math.sqrt(sq)+1)*(Math.sqrt(sq)+1);
+    else
+        return -1;
+}
+// console.log(findNextSquare(625));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
