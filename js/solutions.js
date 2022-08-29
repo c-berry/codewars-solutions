2766a1dfb92414fe445f7d29e51cc5d88b17aa06
Christopher Berry <christopher.m.berry01@gmail.com>
2022-08-29 15:37:22 -0500

    complete daily kata

--- js/solutions.js
+++ js/solutions.js
@@ -1143,3 +1143,52 @@ function firstNonConsecutive(arr) {
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
+// A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+// He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+// His mother looks out of a window 1.5 meters from the ground.
+// How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+// Three conditions must be met for a valid experiment:
+// Float parameter "h" in meters must be greater than 0
+// Float parameter "bounce" must be greater than 0 and less than 1
+// Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1. =>
+function bouncingBall(h,  bounce,  window) {
+    let result = 1;
+    if (!(h > 0 && bounce > 0 && bounce < 1 && window < h)) {
+        return -1;
+    }
+    for (let i = 1; h * bounce > window; i++) {
+        result += 2;
+        h = h * bounce;
+    }
+    return result;
+}
+// console.log(bouncingBall(3.0, 0.66, 1.5));
+// console.log(bouncingBall(30.0, 0.66, 1.5));
+
+// Using recursion:
+function bouncingBall2(h,  bounce,  window) {
+    const cond = h > 0 && (bounce > 0 && bounce < 1) && window < h;
+    if (cond) {
+        // If condition is met, the ball will be seen once going up...
+        // And once coming down, as well as the first time its dropped
+        return h < window ? -1 : 2 + bouncingBall2((h * bounce), bounce, window);
+    } else {
+        return -1;
+    }
+}
+// console.log(bouncingBall2(3.0, 0.66, 1.5));
+// console.log(bouncingBall2(30.0, 0.66, 1.5));
+
+// While loop:
+function bouncingBall3(h,  bounce,  window) {
+    let rebounds = -1;
+    if (bounce > 0 && bounce < 1) while (h > window) rebounds+=2, h *= bounce;
+    return rebounds;
+}
+// console.log(bouncingBall3(3.0, 0.66, 1.5));
+// console.log(bouncingBall3(30.0, 0.66, 1.5));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
