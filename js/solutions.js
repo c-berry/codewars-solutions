4f0a9eea90a81109426c9a5d2547014ae09714f5
Christopher Berry <christopher.m.berry01@gmail.com>
2022-08-30 15:26:58 -0500

    complete daily kata

--- js/solutions.js
+++ js/solutions.js
@@ -1192,3 +1192,26 @@ function bouncingBall3(h,  bounce,  window) {
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+ // Don't change the order of the elements that are left. =>
+function removeSmallest(numbers) {
+    let smallestRating = Number.MAX_SAFE_INTEGER;
+    let index = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] < smallestRating) {
+            smallestRating = numbers[i];
+            index = numbers.indexOf(numbers[i]);
+        }
+    }
+    return numbers.slice(0, index).concat(numbers.slice(index + 1, numbers.length));
+}
+// console.log(removeSmallest([1,2,3,4,5,]));
+// console.log(removeSmallest([5,2,3,4,5,]));
+
+// Using spread operator:
+// numbers.indexOf(Math.min(...numbers))
+// -would work too
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
