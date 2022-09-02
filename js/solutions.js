07393ff9898d4b3498e75050571a6f9d5a41037a
Christopher Berry <christopher.m.berry01@gmail.com>
2022-09-01 21:19:07 -0500

    complete daily kata

--- js/solutions.js
+++ js/solutions.js
@@ -1215,3 +1215,23 @@ function removeSmallest(numbers) {
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
+// function which returns the sum of following series upto nth term(parameter).
+// You need to round the answer to 2 decimal places and return it as String.
+// If the given value is 0 then it should return 0.00
+// You will only be given Natural Numbers as arguments. =>
+function SeriesSum(n) {
+    let sum = 0;
+    for (var i = 0; i < n; i++) {
+        sum += 1 / (3 * i + 1);
+    }
+    return sum.toFixed(2);
+}
+// console.log(SeriesSum(1));
+// console.log(SeriesSum(2));
+// console.log(SeriesSum(3));
+// console.log(SeriesSum(4));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
