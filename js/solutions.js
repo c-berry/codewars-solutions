8d8cc92039b98ad6c4bf3fa22401b2d82c5b7b7f
Christopher Berry <christopher.m.berry01@gmail.com>
2022-09-02 16:34:12 -0500

    complete daly kata

--- js/solutions.js
+++ js/solutions.js
@@ -1235,3 +1235,22 @@ function SeriesSum(n) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 
+// iven an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number. =>
+function sumMix(x) {
+    let result = 0;
+    for (let n of x) {
+        result += parseInt(n);
+    }
+    return result;
+}
+// console.log(sumMix([9, 3, '7', '3']));
+
+// Using map and reduce =>
+function sumMix2(x){
+    return x.map(a => +a).reduce((a, b) => a + b);
+}
+// console.log(sumMix2([9, 3, '7', '3']));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
