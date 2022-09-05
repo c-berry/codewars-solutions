8f5d4c382f6bffed6bfdc17a2f1156d73336e3d8
Christopher Berry <christopher.m.berry01@gmail.com>
2022-09-05 18:43:00 -0500

    complete daily kata

--- js/solutions.js
+++ js/solutions.js
@@ -1254,3 +1254,23 @@ function sumMix2(x){
 
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number. =>
+const quarterOf = (month) => {
+    if (month <= 3) {
+        return 1;
+    } else if (month > 3 && month <= 6) {
+        return 2;
+    } else if (month > 6 && month <= 9) {
+        return 3;
+    } else {
+        return 4;
+    }
+}
+// console.log(quarterOf(1));
+// console.log(quarterOf(5));
+// console.log(quarterOf(12));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
