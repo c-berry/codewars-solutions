16427655c4fb32dd204ac8dd5ba0ca38b5120bf7
Christopher Berry <christopher.m.berry01@gmail.com>
2022-09-15 15:14:18 -0500

    complete daily kata

--- js/solutions.js
+++ js/solutions.js
@@ -1291,3 +1291,17 @@ const number = function (array) {
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 //\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
 
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+// Complete the method which accepts such an array, and returns that single different number. =>
+function stray(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+            return numbers[i];
+        }
+    }
+}
+// console.log(stray([17, 17, 3, 17, 17, 17, 17]));
+
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\
+
