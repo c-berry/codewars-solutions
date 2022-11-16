606760a66c63659cc2922466d77bef1faf72be4f
Christopher Berry <christopher.m.berry01@gmail.com>
2022-11-16 16:48:06 -0600

    modify FizzBuzz for accuracy - switch Fizz and Buzz

--- javajava/FizzBuzz.java
+++ javajava/FizzBuzz.java
@@ -11,9 +11,9 @@ public class FizzBuzz {
             if (i % 15 == 0)
                 System.out.println("FizzBuzz");
             else if (i % 5 == 0)
-                System.out.println("Fizz");
-            else if (i % 3 == 0)
                 System.out.println("Buzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
             else System.out.println(i);
         }
     }
